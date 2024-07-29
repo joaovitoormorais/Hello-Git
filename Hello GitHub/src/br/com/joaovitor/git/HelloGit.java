f8de5a93e355d874ff66c1e056384695bf459cb5
João Vitor de Morais <joao.vpereira@a.ficr.edu.br>
2024-07-29 20:41:01 -0300

    adicionei arquivos via upload

---
+++ Hello GitHub/src/br/com/joaovitor/git/HelloGit.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package br.com.joaovitor.git;
+
+/**
+ * @author joaov
+ * Exemplo de integração do eclipse com o github
+ */
+public class HelloGit {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello Git!");
+	}
+
+}
